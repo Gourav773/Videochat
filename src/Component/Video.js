811a9f31f63798347aef035d97d7aeb2c5d37e1c
gourav <gourav003yadav@gmail.com>
2026-02-22 00:00:23 +0530

    frontend production backend connected

--- src/Component/Video.js
+++ src/Component/Video.js
@@ -58,7 +58,7 @@ export default function Video() {
       formData.append("date", new Date().toISOString().split("T")[0]);
       formData.append("recording", recordedBlob, "recorded-video.webm");
 
-      axios.post("http://localhost:5050/addrecording", formData)
+      axios.post("https://videomeet-1-syct.onrender.com/addrecording", formData)
         .then(() => console.log("Recording uploaded successfully"))
         .catch(console.error);
 
